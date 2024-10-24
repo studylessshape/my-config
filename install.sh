e2a1523177bc79c3505644c2404ff0794ceeba8d
studylessshape <a664554724@qq.com>
2024-10-24 16:29:39 +0700

    chore: add nvim config install

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+# upgrade installed package
 pkg upgrade -y
-apt install -y rust rust-std-wasm32-unknown-unknown python ruff proot-distro neovim nodejs
+# install package for neovim and rust
+apt install -y rust rust-std-wasm32-unknown-unknown python ruff proot-distro neovim nodejs git stylua
+
+# sync git repo
+git pull --recurse-submodules
 
 if [[ -z $(cat ~/.bashrc | grep "~/.cargo/bin") ]]; then
   echo "-- Add cargo bin to PATH"
@@ -7,6 +12,14 @@ if [[ -z $(cat ~/.bashrc | grep "~/.cargo/bin") ]]; then
 else
   echo "-- Already has cargo bin PATH"
 fi
+
+echo "-- Install wasm runner"
+cargo install wasm-server-runner
+
+echo "-- Copy nvim config files"
+cp ~/.config/nvim ~/.config/nvim.bak -r
+rm -rf ~/.config/nvim
+cp ./nvim ~/.config/nvim -r
 
 if [ ".termux" = "$(ls ~ -a | grep .termux)" ]; then
   echo "-- Backup termux settings"
@@ -18,7 +31,7 @@ else
 fi
 
 echo "-- Copy settings"
-cp ./styling/* ~/.termux/
+cp ./termux/styling/* ~/.termux/
 
 echo "-- Reload settings"
 termux-reload-settings
